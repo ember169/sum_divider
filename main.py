bc0a2041031d18ca9a6959f18b7b6e07ff852495
ember169 <expressopossum@proton.me>
2024-11-09 22:15:04 +0100

    text-align

--- main.py
+++ main.py
@@ -85,7 +85,6 @@ html_template = """
             padding: 20px;
             border-radius: 8px;
             box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.5);
-            text-align: center;
             margin-bottom: 20px;
         }
         .total-row {
