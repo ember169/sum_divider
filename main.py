33f0a83546fbef6488ab8cbe82a1fd99333fb028
ember169 <182413924+ember169@users.noreply.github.com>
2024-11-10 08:47:47 +0100

    adding eur conversion

--- main.py
+++ main.py
@@ -10,16 +10,12 @@ html_template = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Répartition des gains</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@500&display=swap" rel="stylesheet">
     <style>
-        /* Applique l'overlay sombre par-dessus le GIF en fond */
+        /* Styles CSS */
         body {
             font-family: 'Roboto', sans-serif;
-            background-image: 
-                linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), /* Overlay sombre */
-                url('https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3BnZGg4OGpjM2JueWE4c2F1Mm50MWgyNWUwNnkzN2ljbHdjOGU5biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YnkMcHgNIMW4Yfmjxr/giphy.gif'); /* GIF en fond */
-            background-size: cover;
-            background-position: center;
+            background-color: #121212;
             color: #e0e0e0;
             display: flex;
             flex-direction: column;
@@ -28,33 +24,21 @@ html_template = """
             min-height: 100vh;
             margin: 0;
         }
-        h2 {
-            font-family: 'Roboto', sans-serif;
-            font-weight: 500;
-            color: #ffffff;
+        h3 {
             text-align: center;
         }
         form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            max-width: 400px;
-            width: 90%;
             background-color: #1f1f1f;
             padding: 20px;
             border-radius: 8px;
             box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.5);
             margin-bottom: 20px;
-            text-align: center;
-            font-weight: 400;
+            max-width: 400px;
+            width: 90%;
         }
         label, input, button {
-            font-family: 'Roboto', sans-serif;
             width: 100%;
-            color: #e0e0e0;
-            font-weight: 500;
             margin-bottom: 15px;
-            max-width: 300px;
         }
         input {
             background-color: #333;
@@ -67,16 +51,11 @@ html_template = """
         }
         button {
             padding: 12px;
-            background-color: #1B448F;
+            background-color: #4CAF50;
             color: white;
             border: none;
             border-radius: 5px;
             cursor: pointer;
-            text-align: center;
-            max-width: 300px;
-        }
-        button:hover {
-            background-color: #0C1D3E;
         }
         .results {
             max-width: 400px;
@@ -85,70 +64,68 @@ html_template = """
             padding: 20px;
             border-radius: 8px;
             box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.5);
-            margin-bottom: 20px;
-        }
-        .total-row {
-            font-weight: 700;
-            background-color:#333;
+            text-align: center;
         }
         table {
             width: 100%;
             border-collapse: collapse;
             font-family: 'Roboto Mono', monospace;
-            font-weight: 400!important;
         }
         th, td {
             padding: 12px;
+            text-align: right;
             border: 1px solid #333;
         }
         th {
             background-color: #333;
             color: #e0e0e0;
         }
-        td {
-            color: #ffffff;
-        }
-        .right-align {
-            text-align: right;
-        }
     </style>
 </head>
 <body>
     <form method="post">
-        <h3>Montant à répartir</h3>
-        <input type="text" name="montant" placeholder="Entrez le montant" required pattern="\\d+" title="Entrez un nombre entier">
-        <button type="submit">Calculer</button>
+        <h3>Montant total en USDC</h3>
+        <input type="text" name="montant_usdc" placeholder="Entrez le montant en USDC" required pattern="\\d+(\\.\\d{1,2})?" title="Entrez un montant valide" value="{{ montant_usdc }}">
+        
+        {% if montant_placeholder %}
+            <h3>Montant restant en EUR (hors crypto)</h3>
+            <input type="text" name="montant_eur" placeholder="Suggestion : {{ montant_placeholder }}" required pattern="\\d+(\\.\\d{1,2})?" title="Entrez un montant valide" value="{{ montant_eur }}">
+            <button type="submit" formaction="/calculer">Valider les montants</button>
+        {% else %}
+            <button type="submit" formaction="/calcul_placeholder">Calculer le montant EUR suggéré</button>
+        {% endif %}
     </form>
+    
     {% if result %}
     <div class="results">
         <table>
             <tr class="total-row">
                 <td>Montant total</td>
-                <td class="right-align">{{ result['montant_total'] }}</td>
+                <td>{{ result['montant_usdc'] }}</td>
             </tr>
             <tr>
                 <td>Crypto</td>
-                <td class="right-align">{{ result['crypto'] }}</td>
+                <td>{{ result['crypto'] }}</td>
             </tr>
             <tr class="total-row">
                 <td>Montant imposable</td>
-                <td class="right-align">{{ result['imposable'] }}</td>
+                <td>{{ result['montant_eur'] }}</td>
             </tr>
             <tr class="taxable-row">
                 <td>PEA</td>
-                <td class="right-align">{{ result['pea'] }}</td>
+                <td>{{ result['pea'] }}</td>
             </tr>
             <tr class="taxable-row">
                 <td>LA</td>
-                <td class="right-align">{{ result['epargne'] }}</td>
+                <td>{{ result['epargne'] }}</td>
             </tr>
             <tr class="taxable-row">
                 <td>LDDS (impôts)</td>
-                <td class="right-align">{{ result['impots'] }}</td>
+                <td>{{ result['impots'] }}</td>
             </tr>
             <tr class="taxable-row">
                 <td>Cash</td>
-                <td class="right-align">{{ result['cash'] }}</td>
+                <td>{{ result['cash'] }}</td>
             </tr>
         </table>
     </div>
@@ -158,36 +135,51 @@ html_template = """
 """
 
 @app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template_string(html_template, montant_placeholder=None, result=None)
+
+@app.route("/calcul_placeholder", methods=["POST"])
+def calcul_placeholder():
+    try:
+        montant_usdc = float(request.form["montant_usdc"])
+        crypto = round(montant_usdc * 0.10, 2)
+        montant_placeholder = round(montant_usdc - crypto, 2)
+        return render_template_string(html_template, montant_usdc=montant_usdc, montant_placeholder=montant_placeholder, result=None)
+    except ValueError:
+        return render_template_string(html_template, montant_placeholder=None, result=None)
+
+@app.route("/calculer", methods=["POST"])
 def repartir_gains():
-    result = None
-    if request.method == "POST":
-        try:
-            montant = float(request.form["montant"])
-            
-            # Calculs pour chaque enveloppe
-            crypto = round(montant * 1 / 10, 2)
-            montant_sans_crypto = montant - crypto
-            impots = math.ceil(montant_sans_crypto * 0.30)
-            reste = montant_sans_crypto - impots
-            cash = round(reste * 1 / 10, 2)
-            pea = round(reste * 4 / 10, 2)
-            epargne = round(reste * 2 / 10, 2)
+    try:
+        montant_usdc = float(request.form["montant_usdc"])
+        montant_eur = float(request.form["montant_eur"])
 
-            def format_number(value):
-                return "{:,.2f}".format(value).replace(",", " ").replace(".", ",")
+        # Calcul de la part crypto (10 % du montant total en USDC)
+        crypto = round(montant_usdc * 0.10, 2)
 
-            result = {
-                "montant_total": format_number(montant),
-                "imposable": format_number(montant_sans_crypto),
-                "cash": format_number(cash),
-                "pea": format_number(pea),
-                "epargne": format_number(epargne),
-                "impots": format_number(impots),
-                "crypto": format_number(crypto),
-            }
-        except ValueError:
-            result = None
-    return render_template_string(html_template, result=result)
+        # Calcul du montant imposable (le montant EUR entré manuellement)
+        impots = math.ceil(montant_eur * 0.30)
+        reste = montant_eur - impots
+        cash = round(reste * 0.10, 2)
+        pea = round(reste * 0.40, 2)
+        epargne = round(reste * 0.20, 2)
+
+        # Formattage des montants
+        def format_number(value):
+            return "{:,.2f}".format(value).replace(",", " ").replace(".", ",")
+
+        result = {
+            "montant_usdc": format_number(montant_usdc),
+            "crypto": format_number(crypto),
+            "montant_eur": format_number(montant_eur),
+            "cash": format_number(cash),
+            "pea": format_number(pea),
+            "epargne": format_number(epargne),
+            "impots": format_number(impots),
+        }
+        return render_template_string(html_template, montant_placeholder=None, result=result)
+    except ValueError:
+        return render_template_string(html_template, montant_placeholder=None, result=None)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
